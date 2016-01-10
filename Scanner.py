3519e28945d6ccdde63906e4dd396673f5a45890
AbdielMelendez/ <abdielhmelendez@gmail.com>
2016-01-09 17:37:39 -0800

    Update

--- Scanner.py
+++ Scanner.py
@@ -1,3 +1,12 @@
- import os
+import os
+
+def make_dir(directory):
+   if not os.path.exist(directory):
+       os.makedirs(directory)
+
+def write_file(path, data):
+    f = open(path)
+    f.write(data)
+    f.close()
 
 
